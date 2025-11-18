cb3d64a9b52b09b4ab286982eef7658354f98a82
garden2002 <ehxhfl1017@naver.com>
2025-11-18 22:55:23 +0900

    class Rock 생성

---
+++ Server/Rock.cpp
@@ -0,0 +1,15 @@
+﻿#include "Rock.h"
+
+Rock::Rock()
+{
+}
+
+Rock::~Rock()
+{
+}
+
+void Rock::SetDirection(const XMFLOAT3& PlayerPosition)
+{
+	XMFLOAT3 Direction = Vector3::Subtract(PlayerPosition, m_Xmf3Position);
+	m_Xmf3Direction = Vector3::Normalize(Direction);
+}
---
+++ Server/Rock.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "Common.h"
+//////////////////////////////////////////////////////////////////////////////////////////////
+// 2025.11.18
+// Class Rock By 민정원
+// Class Rock 정의
+class Rock 
+{
+public:
+	Rock();
+	~Rock();
+	void SetDirection(const XMFLOAT3& PlayerPosition);
+	const XMFLOAT3& GetPosition(){ return m_Xmf3Position; }
+private:
+	float speed = 10.f;
+	XMFLOAT3 m_Xmf3Direction{};
+	XMFLOAT3 m_Xmf3Position{};
+};
+
+
